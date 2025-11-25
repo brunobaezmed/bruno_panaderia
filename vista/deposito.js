5b3f008066442730a8a9760de59e9778863edc51
brunobaezmed <brunomatias33@gmail.com>
2025-11-25 18:15:47 -0300

    deleted:    controladores/deposito.php 	deleted:    paginas/referenciales/deposito/diag.php 	deleted:    paginas/referenciales/deposito/listar.php 	deleted:    vista/deposito.js

--- vista/deposito.js
+++
@@ -1,2 +0,0 @@
-// Depósito: eliminado. Este archivo ha sido vaciado por petición del usuario.
-// Si necesita restaurar la funcionalidad de depósitos, reponga el archivo original o solicite restauración.
